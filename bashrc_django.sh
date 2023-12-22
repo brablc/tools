b7f487f3898155d762f8e2bc5d45acdaeaf4fc4d
Ondrej Brablc <ondrej@brablc.com>
2023-12-22 11:50:03 +0100

    Introduce dj_liverun

--- bashrc_django.sh
+++ bashrc_django.sh
@@ -13,23 +13,27 @@ function vactivate() {
         dir="$(dirname "$dir")"
     done
     echo "No .venv directory found."
+    return 1
 }
+export -f vactivate
 
 function _dj_init() {
     [[ -n "${VIRTUAL_ENV:-}" ]] || vactivate
     export DYLD_FALLBACK_LIBRARY_PATH=$(brew --prefix libpq)/lib:$DYLD_FALLBACK_LIBRARY_PAT
 }
+export -f _dj_init
 
 function dj() {
-    while [[ ! -f manage.py && ! -d .django-dir && "$PWD" != "$HOME" ]]; do
+    while [[ ! -f manage.py && ! -f .django-dir && "$PWD" != "$HOME" ]]; do
         cd ..
     done
-    if [[ -d .django-dir ]]; then
-        cd $(readlink .django-dir)
+    if [[ -f .django-dir ]]; then
+        cd $(cat .django-dir)
     fi
     _dj_init
     python manage.py "$@"
 }
+export -f dj
 
 _dj() {
     _dj_init
@@ -44,3 +48,36 @@ _dj() {
     return 0
 }
 complete -F _dj dj
+
+
+function dj_liverun() {
+
+    vactivate || return 1
+    _dj_init
+
+    let session_name='dj_liverun'
+
+    if ! tmux has-session -t $session_name 2>/dev/null; then
+
+        tmux new-session -d -s $session_name
+
+        tmux split-window -v
+        tmux split-window -v
+        tmux select-pane -t 2 # Select Pane 2 (bottom pane)
+        tmux split-window -h  # Split Pane 2 horizontally: creates Pane 3 beside Pane 2
+
+        tmux select-pane -t 1
+        tmux send-keys 'while true; do dj runserver 0.0.0.0:8000; done' C-m
+
+        tmux select-pane -t 2
+        tmux send-keys 'dj livereload' C-m
+
+        tmux select-pane -t 3
+        tmux resize-pane -R 20
+        tmux send-keys 'npm run tailwind' C-m
+
+        tmux select-pane -t 0
+    fi
+
+    tmux attach-session -t $session_name
+}
